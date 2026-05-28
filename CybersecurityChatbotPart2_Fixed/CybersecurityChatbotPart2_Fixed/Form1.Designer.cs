3b8e64e3b1902bf03ae20d2a0115aa8c8bbf35c6
uhonemuedi <ST10474866@rcconnect.edu.za>
2026-05-29 01:07:22 +0200

    Added Part 2 WinForms GUI chatbot with keyword recognition, sentiment detection , memory recall and random responses

---
+++ CybersecurityChatbotPart2_Fixed/CybersecurityChatbotPart2_Fixed/Form1.Designer.cs
@@ -0,0 +1,28 @@
+﻿namespace CybersecurityChatbotPart2_Fixed
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 650);
+            this.Name = "Form1";
+            this.Text = "Cybersecurity Awareness Chatbot";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+        }
+    }
+}
---
+++ CybersecurityChatbotPart2_Fixed/CybersecurityChatbotPart2_Fixed/Form1.cs
@@ -0,0 +1,10 @@
+namespace CybersecurityChatbotPart2_Fixed
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
